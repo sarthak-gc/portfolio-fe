669951db6a18fc300c28031c71acf8d4bb76a4f2
sarthak-gc <sarthak-gc@users.noreply.github.com>
2025-05-11 02:03:46 +0545

    ...

--- frontend/src/Components/Projects/Project/Links.tsx
+++ frontend/src/Components/Projects/Project/Links.tsx
@@ -1,6 +1,6 @@
 import { DemoLinkLogo } from "@/Components/Logos/DemoLinkLogo";
 import { GithubLogo } from "@/Components/Logos/GithubLogo";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import {
   Tooltip,
   TooltipContent,
